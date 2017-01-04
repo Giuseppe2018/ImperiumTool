aaeba7b47e5285e15306e3316725aed399da3733
xKryptus <dar.m.clarke@gmail.com>
2017-01-04 02:06:54 -0800

    Removed unnecessary namespaces

--- Imperium/Main.cs
+++ Imperium/Main.cs
@@ -2,20 +2,15 @@
 using PS3Lib;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
-using System.Text.RegularExpressions;
-using System.Runtime.InteropServices;
 using System.Net.NetworkInformation;
-using System.Web.Script.Serialization;
 
 namespace Imperium
 {
